1de49030bd8e6b50d88d0d5055f2d61d7ab806f8
kzcheng <kevinzifancheng@gmail.com>
2020-04-28 10:28:57 -0700

    Completed exercises

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -1,11 +1,13 @@
-require_relative '../setup'
-require_relative './exercise_1'
-require_relative './exercise_2'
-require_relative './exercise_3'
-require_relative './exercise_4'
-require_relative './exercise_5'
+require_relative "../setup"
+require_relative "./exercise_1"
+require_relative "./exercise_2"
+require_relative "./exercise_3"
+require_relative "./exercise_4"
+require_relative "./exercise_5"
 
 puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store2.employees.create(first_name: "Akira", last_name: "Cheng", hourly_rate: 4000)
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -1,12 +1,16 @@
-require_relative '../setup'
-require_relative './exercise_1'
-require_relative './exercise_2'
-require_relative './exercise_3'
-require_relative './exercise_4'
-require_relative './exercise_5'
-require_relative './exercise_6'
+require_relative "../setup"
+require_relative "./exercise_1"
+require_relative "./exercise_2"
+require_relative "./exercise_3"
+require_relative "./exercise_4"
+require_relative "./exercise_5"
+require_relative "./exercise_6"
 
 puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+store_name = gets.chomp
+new_store = Store.create(name: store_name)
+new_store.validate!
+# pp new_store.errors.full_messages
